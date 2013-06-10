8fd5ff25f5c3fd606871c3cd6ee36fa9c1704ad2
Henrique Dantas <hndantas@gmail.com>
2013-06-10 03:13:20 +0200

    Basically finished assignmentA PThreads

--- A/ass1-pthreads.c
+++ A/ass1-pthreads.c
@@ -4,8 +4,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define NUM_THREADS 2
-#define MAX_INPUT_SIZE 32
+#define NUM_THREADS 3
+#define MAX_INPUT_SIZE 64
 #define NELEMS(x)  (sizeof(x) / sizeof(x[0])) // number of elements in an array
 
 
@@ -20,62 +20,31 @@ Keep trying to assign tasks to threads while there are uncomplete tasks.
 Done! Print result and terminate.
 
 ********************************************************************/
-struct thread_data{
-	struct thread_data* left;
-	struct thread_data* right;
-	int leftsize;
-	int rightsize;
+typedef struct thread_data{
+	struct thread_data* leftstruct;
+	struct thread_data* rightstruct;
+	int size;
 	int* pm;
 	int* sm;
-};
+} thread_data;
 
-struct thread_data thread_data_array[NUM_THREADS];
-
-struct thread_data* queue1[MAX_INPUT_SIZE];
-struct thread_data* queue2[MAX_INPUT_SIZE];
 
 
 
 void handlerc(int rc, char* string, long t);
-int* prefixMinima (int* left, int* right, int sizeleft, int sizeright);
-int* suffixMinima (int* left, int* right, int sizeleft, int sizeright);
-void pushToQueue (struct thread_data** queue, long* counter, struct thread_data *value);
-struct thread_data* pullFromQueue (struct thread_data** queue,long *counter);
-//void *ComputeMinima(void *threadarg);
+int* prefixMinima (thread_data* leftstruct, thread_data* rightstruct, int leftsize, int rightsize);
+int* suffixMinima (thread_data* leftstruct, thread_data* rightstruct, int leftsize, int rightsize);
+void pushToQueue (thread_data** queue, volatile long* counter, thread_data *value);
+thread_data* pullFromQueue (thread_data** queue, volatile long *counter);
+void *ComputeMinima(void *threadarg);
 
-
-
-
-void *ComputeMinima(void *threadarg)
-{
-	return (void *) NULL;
-	/*int i;
-	struct thread_data *my_data;
-	sleep(1);
-	my_data = (struct thread_data *) threadarg;
-	
-
-	my_data->pm = prefixMinima (my_data->left,my_data->right,my_data->leftsize,my_data->rightsize);//CHANGE THIS
-	my_data->sm = suffixMinima (my_data->left,my_data->right,my_data->leftsize,my_data->rightsize);
-	
-	printf("\nStarting:\t");
-	printf("\npreffixMinima:\t");
-	for (i = 0; i<6; i++)
-		printf("%d%c ",*(my_data->pm+i),(i==5)?'\n':','); // the weird thing in the printf is to avoid having comma in the last cycle
-
-	printf("\bsuffixMinima:\t");
-	for (i = 0; i<6; i++)
-		printf("%d%c ",*(my_data->sm+i),(i==5)?'\n':',');
-
-	return (void *) my_data;*/
-}
 
 
 
 int main (int argc, char *argv[]) {
 
-	int array[] = {1,2,3,4,5,6,7,8,9};
-	// int array[] = {58,89,32,73,131,156,30,29,141,37,133,151,88,53,122,126,131,49,130,115,16,83,40,145,10,112,20,147,14,104,111,92};
+	// int array[] = {58,89,32,73,131,156,30,29};
+	int array[] = {58,89,32,73,131,156,30,29,141,37,133,151,88,53,122,126,131,49,130,115,16,83,40,145,10,112,20,147,14,104,111,92};
 	pthread_t threads[NUM_THREADS];
 	pthread_attr_t attr;
 
@@ -86,14 +55,29 @@ int main (int argc, char *argv[]) {
 	int heightTree = ceil(log2(arraysize));
 	int elementsperlevel[heightTree];
 
-	struct thread_data **thislevelqueue_ptr = queue1;
-	struct thread_data **nextlevelqueue_ptr = queue2;
+	thread_data thread_data_instances[NUM_THREADS] = {{}}; //let compiler initialize all struct instances to zeros or NULL. ugly :S
+	thread_data* thread_data_array[NUM_THREADS];
+	
+
+	thread_data* queue1[MAX_INPUT_SIZE] = {0};
+	thread_data* queue2[MAX_INPUT_SIZE] = {0};
+	thread_data* queueZEROS[MAX_INPUT_SIZE] = {0};
+
+
+
+	thread_data **thislevelqueue_ptr = queue1;
+	thread_data **nextlevelqueue_ptr = queue2;
+
 	volatile long thislevelqueue_cnt = 0; // volatile to prevent infinite loops later
 	volatile long nextlevelqueue_cnt = 0; // volatile to prevent infinite loops later
 
+	for (i=0;i<NUM_THREADS;i++){ //thread_data_array contains pointers to each instance in the thread_data_instances
+		thread_data_array[i] = &(thread_data_instances[i]);
+	}
+
 
 	j = arraysize;
-	for (k=0;k<heightTree;k++){
+	for (k = 0; k < heightTree; k++){
 		elementsperlevel[k]=j;
 
 		if (j%2 == 0)
@@ -102,65 +86,71 @@ int main (int argc, char *argv[]) {
 			j = floor(j/2)+1;
 	}
 
-	for (i=0;i<NELEMS(elementsperlevel);i++){
-		printf("elementsperlevel[%d]:%d\n",i,elementsperlevel[i]);
-	}
+	// for (i = 0; i < NELEMS(elementsperlevel); i++){
+	// 	printf("elementsperlevel[%d]:%d\n",i,elementsperlevel[i]);
+	// }
 	
 	/* Initialize and set thread joinable attribute */
 	pthread_attr_init(&attr);
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 	
-	int elementbeingprocessed = 0;
-	int processedelements = 0;
+	thread_data* tmp_struct_ptr;
 
-	
-	// FILL this_level_queue FOR THE 1ST TIME, CONVERT ARRAY VALUES TO STRUCT DATA
+	// initialize queue with input array
+	for (i = 0; i < elementsperlevel[0]; i++){
+		tmp_struct_ptr = (thread_data* ) malloc(sizeof(*tmp_struct_ptr)); //allocate space for a new thread_data struct
+		/*tmp_struct.leftstruct = NULL;
+		tmp_struct.rightstruct = NULL;*/
+		*tmp_struct_ptr = (thread_data) {.leftstruct=NULL,.rightstruct=NULL,.size = 1,.pm = array + i,.sm = array + i};
+		pushToQueue(thislevelqueue_ptr,&thislevelqueue_cnt,tmp_struct_ptr);
+	}
+	free(tmp_struct_ptr);
+	thislevelqueue_cnt = 0;
 
-
-	for (k=0;k<heightTree;k++){
+	for (k = 0; k < heightTree; k++){
 		while(thislevelqueue_cnt < elementsperlevel[k]){
 			
 			for (t1 = 0; t1 < NUM_THREADS; t1++){ //spawn new threads
 				if (thislevelqueue_cnt >= (elementsperlevel[k] - 1)){ // in case there are not enough unprocess inputs
-					t1--; //"interrupt" this thread
-					printf("\nBREAK:T1=%ld\tT2=%ld\t\tthislevelqueue_cnt:%ld\tnextlevelqueue_cnt:%ld\n",t1,t2,thislevelqueue_cnt,nextlevelqueue_cnt);	
+					// printf("\nBREAK:T1=%ld\t\tthislevelqueue_cnt:%ld\tnextlevelqueue_cnt:%ld\n",t1,thislevelqueue_cnt,nextlevelqueue_cnt);	
 					break;
 				}
-				//thread_data_array[t1].left= pullFromQueue(&thislevelqueue_ptr,&thislevelqueue_cnt);
-				thislevelqueue_cnt++;
-				//thread_data_array[t1].right= pullFromQueue(&thislevelqueue_ptr,&thislevelqueue_cnt);
-				thislevelqueue_cnt++;
-				printf("Creating thread %ld in level%d.\n",t1,k);
-				/*rc = pthread_create(&threads[t1], &attr, ComputeMinima, (void *) &thread_data_array[t1]);
-				handlerc(rc,"create",t1);*/
+				thread_data_array[t1]->leftstruct= pullFromQueue(thislevelqueue_ptr,&thislevelqueue_cnt);
+				thread_data_array[t1]->rightstruct= pullFromQueue(thislevelqueue_ptr,&thislevelqueue_cnt);
+				// printf("\tCreating thread %ld in level%d.\t",t1,k);
 				
-				printf("SPAWN:T1=%ld\tT2=%ld\t\tthislevelqueue_cnt:%ld\tnextlevelqueue_cnt:%ld\n",t1,t2,thislevelqueue_cnt,nextlevelqueue_cnt);
+				rc = pthread_create(&threads[t1], &attr, ComputeMinima, (void *) thread_data_array[t1]);
+				handlerc(rc,"create",t1);
+				
+				// printf("SPAWN:T1=%ld\t\tthislevelqueue_cnt:%ld\tnextlevelqueue_cnt:%ld\n",t1,thislevelqueue_cnt,nextlevelqueue_cnt);
 			}
-			printf("\n");
+
+			// printf("T1:%ld\tT2:%ld\n",t1,t2);
+			
 			for (t2 = 0; t2 < t1; t2++){ //merge spawned threads
-				printf("MERGE:T1=%ld\tT2=%ld\t\tthislevelqueue_cnt:%ld\tnextlevelqueue_cnt:%ld\n",t1,t2,thislevelqueue_cnt,nextlevelqueue_cnt);
-				printf("Joining thread %ld in level%d.\n",t2,k);
-				/*rc = pthread_join(threads[t2], &status);
-				handlerc(rc,"join",t2);*/
-				//pushToQueue(&nextlevelqueue_ptr,&nextlevelqueue_cnt,(struct thread_data *)status); //push obtained results to queue
-				nextlevelqueue_cnt++;
-				printf("MERGE:T1=%ld\tT2=%ld\t\tthislevelqueue_cnt:%ld\tnextlevelqueue_cnt:%ld\n",t1,t2,thislevelqueue_cnt,nextlevelqueue_cnt);
+				// printf("\tJoining thread %ld in level%d.\t",t2,k);
+				
+				rc = pthread_join(threads[t2], &status);
+				handlerc(rc,"join",t2);
+				
+				pushToQueue(nextlevelqueue_ptr,&nextlevelqueue_cnt,(thread_data *)status); //push obtained results to queue
+				// printf("MERGE:T2=%ld\t\tthislevelqueue_cnt:%ld\tnextlevelqueue_cnt:%ld\n",t2,thislevelqueue_cnt,nextlevelqueue_cnt);
 			}
 
 			if (thislevelqueue_cnt == elementsperlevel[k] - 1){ //in case there is a unpaired element at the end of the input array
-				//thread_data_array[0].left= pullFromQueue(&thislevelqueue_ptr,&thislevelqueue_cnt);
-				thislevelqueue_cnt++;
-				//pushToQueue(&nextlevelqueue_ptr,&nextlevelqueue_cnt,&thread_data_array[0]); //push unpaired element to queue
-				nextlevelqueue_cnt++;
-				printf("\nREMAINDER:T1=%ld\tT2=%ld\t\tthislevelqueue_cnt:%ld\tnextlevelqueue_cnt:%ld\n",t1,t2,thislevelqueue_cnt,nextlevelqueue_cnt);
+				thread_data_array[0]= pullFromQueue(thislevelqueue_ptr,&thislevelqueue_cnt);
+				pushToQueue(nextlevelqueue_ptr,&nextlevelqueue_cnt,thread_data_array[0]); //push unpaired element to queue
+				// printf("\nREMAINDER:T1=%ld\tT2=%ld\t\tthislevelqueue_cnt:%ld\tnextlevelqueue_cnt:%ld\n",t1,t2,thislevelqueue_cnt,nextlevelqueue_cnt);
 			}
+			// printf("\n");
 
-			printf("\n######\n\n");
 		}
 
-		printf("Resetting variables\n");
+		// printf("\n######\n\n");
 		//next level queue becomes current level
 		thislevelqueue_ptr = nextlevelqueue_ptr;
+		nextlevelqueue_ptr = queueZEROS;
+		
 		thislevelqueue_cnt = 0;
 		nextlevelqueue_cnt = 0;
 		
@@ -170,50 +160,74 @@ int main (int argc, char *argv[]) {
 	// Free attribute
 	pthread_attr_destroy(&attr);
 
-	/*printf("Main preffixMinima:\t");
-	for (i = 0; i<6; i++)
-		printf("%d%c ",*(thread_data_array[NUM_THREADS-1].pm+i),(i==5)?'\n':',');
+
+	printf("\ni:\t\t\t");
+	for (i = 1; i<=arraysize; i++)
+		if (i<10)
+			printf("0%d%c ",i,(i==arraysize)?'\n':',');
+		else
+			printf("%d%c ",i,(i==arraysize)?'\n':',');
+
+	printf("\bMain preffixMinima:\t");
+	for (i = 0; i<arraysize; i++)
+		printf("%d%c ",*(nextlevelqueue_ptr[0]->pm+i),(i==(arraysize-1))?'\n':',');
 
 	printf("\bMain suffixMinima:\t");
-	for (i = 0; i<6; i++)
-		printf("%d%c ",*(thread_data_array[NUM_THREADS-1].sm+i),(i==5)?'\n':',');*/
+	for (i = 0; i<arraysize; i++)
+		printf("%d%c ",*(nextlevelqueue_ptr[0]->sm+i),(i==(arraysize-1))?'\n':',');
 
 	printf("\bMain: program completed. Exiting.\n");
-	// Last thing that main() should do 
-	pthread_exit(NULL);
+	
+	return(0);
 
 }
 
 
-int* prefixMinima (int* left, int* right, int sizeleft, int sizeright) {
+int* prefixMinima (thread_data* leftstruct,thread_data* rightstruct, int sizeleft, int sizeright) {
+	int* left = leftstruct->pm;
+	int* right = rightstruct->pm;
+
+	int min;
 	int lastleft = *(left+sizeleft-1);
 	int firstright = *right;
 	int* outputarray = malloc((sizeleft+sizeright)*sizeof(int));
 	int i;
-
 	memcpy(outputarray,left,sizeleft*sizeof(int));
 	if (lastleft >= firstright) //concatenate arrays
 		memcpy(outputarray+sizeleft,right,sizeright*sizeof(int)); //append right array to output
-	else { //replace the complete right array with lastleft
-		for (i=0; i<sizeright; i++)
-			*(outputarray+sizeleft+i)=lastleft;
+	else {
+		min = lastleft;
+		for (i=0; i<sizeright; i++) {
+			if (*(right+i) < min)
+				min = *(right+i);
+
+			*(outputarray+sizeleft+i)=min;
+		}
 	}
 
 	return outputarray;
 }
 
-int* suffixMinima (int* left, int* right, int sizeleft, int sizeright) {
+int* suffixMinima (thread_data* leftstruct, thread_data* rightstruct, int sizeleft, int sizeright) {
+	int* left = leftstruct->sm;
+	int* right = rightstruct->sm;
+
+	int min;
 	int lastleft = *(left+sizeleft-1);
 	int firstright = *right;
 	int* outputarray = malloc((sizeleft+sizeright)*sizeof(int));
 	int i;
-
 	memcpy(outputarray+sizeleft,right,sizeright*sizeof(int));
 	if (lastleft <= firstright) //concatenate arrays
 		memcpy(outputarray,left,sizeleft*sizeof(int)); //preppend left array to output
-	else { //replace the complete left array with firstright
-		for (i=0; i<sizeleft; i++)
-			*(outputarray+i)=firstright;
+	else {
+		min = firstright;
+		for (i=sizeleft-1; i>=0; i--) {
+			if(*(left+i) < min)
+				min = *(left+i);
+
+			*(outputarray+i)=min;
+		}
 	}
 
 	return outputarray;
@@ -226,15 +240,42 @@ void handlerc(int rc,char* string, long t){
 	}
 }
 
-void pushToQueue (struct thread_data** queue, long* counter, struct thread_data *value){
-	long cnt = *counter;
-	*(queue + cnt) = value;
-	*counter = cnt + 1;
+void pushToQueue (thread_data** queue, volatile long* counter, thread_data *value){
+	*(queue + *counter) = value;
+	*counter = *counter + 1;
 }
 
-struct thread_data* pullFromQueue (struct thread_data** queue,long *counter){
-	long cnt = *counter;
-	*counter = cnt + 1;
-	return *(queue + cnt);
+thread_data* pullFromQueue (thread_data** queue, volatile long *counter){
+	*counter = *counter + 1;
+	return *(queue + *counter - 1);
 }
 
+
+void *ComputeMinima(void *threadarg)
+{
+	int tmp_size = 0;
+	int* tmp_pm;
+	int* tmp_sm;
+
+	thread_data *new_threadarg = (thread_data*) threadarg;  //typecast threadarg
+	thread_data *new_struct;
+	new_struct = (thread_data*) malloc(sizeof(*new_struct)); //allocate space for a new thread_data struct
+	 
+	
+	tmp_pm = prefixMinima(new_threadarg->leftstruct,new_threadarg->rightstruct,new_threadarg->leftstruct->size,new_threadarg->rightstruct->size);
+	tmp_sm = suffixMinima(new_threadarg->leftstruct,new_threadarg->rightstruct,new_threadarg->leftstruct->size,new_threadarg->rightstruct->size);
+	tmp_size = (new_threadarg->leftstruct->size+new_threadarg->rightstruct->size);
+
+
+	*new_struct = (thread_data) {.leftstruct=new_threadarg->leftstruct,.rightstruct=new_threadarg->rightstruct,.size = tmp_size,.pm = tmp_pm,.sm = tmp_sm};
+
+	// printf("\t\tpreffixMinima:\t");
+	// for (i = 0; i < tmp_size; i++)
+	// 	printf("%d%c ",*(new_struct->pm+i),(i==(tmp_size-1))?'\n':','); // the weird thing in the printf is to avoid having comma in the last cycle
+
+	// printf("\t\tsuffixMinima:\t");
+	// for (i = 0; i< tmp_size; i++)
+	// 	printf("%d%c ",*(new_struct->sm+i),(i==(tmp_size-1))?'\n':',');
+	// printf("new_struct=%p\n",new_struct);
+	return (void *) new_struct;
+}
